8d6fbc5c72873866928dc3c2e2054908d6783e93
k0bary0 <ryota.drums.3160@gmail.com>
2024-06-29 15:58:16 +0900

    Implement basic login

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
 //= link application.js
  //= link controllers/application.js
  //= link controllers/hello_controller.js
